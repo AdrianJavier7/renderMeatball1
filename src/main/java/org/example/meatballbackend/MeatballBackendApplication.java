f108bf31336c2a2542273d3a07c27bed73ac21e4
AdrianJavier7 <aperezmiranda@safareyes.es>
2025-01-14 17:10:25 +0100

    Empezamos

---
+++ src/main/java/org/example/meatballbackend/MeatballBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.meatballbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatballBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatballBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/meatballbackend/MeatballBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meatballbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatballBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

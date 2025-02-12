ebc1d280550e7c5e4c6b107a72b1ca1635fe51da
Jose Maria <jmateosmartin@safareyese.es>
2025-02-12 17:58:18 +0100

    Enviar email terminado

--- src/main/java/org/example/meatballbackend/Controladores/AuthController.java
+++ src/main/java/org/example/meatballbackend/Controladores/AuthController.java
@@ -5,7 +5,9 @@ import org.example.meatballbackend.Dto.LoginDTO;
 import org.example.meatballbackend.Dto.RegistroDTO;
 import org.example.meatballbackend.Dto.RespuestaDTO;
 import org.example.meatballbackend.Entidades.Usuario;
+import org.example.meatballbackend.Servicios.EmailService;
 import org.example.meatballbackend.Servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,9 @@ public class AuthController {
 
     private UsuarioService service;
 
+    @Autowired
+    private EmailService emailService;
+
     @PostMapping("/registro/perfil")
     public Usuario registro(@RequestBody RegistroDTO registroDTO){
         return service.registrarUsuario(registroDTO);
@@ -29,5 +34,9 @@ public class AuthController {
         return service.login(dto);
     }
 
-    
+    @PostMapping("/enviarEmail")
+    public ResponseEntity<String> registerUser(@RequestBody Usuario user) {
+        emailService.sendEmail(user.getEmail(), "Bienvenido a la App", "Gracias por registrarte!");
+        return ResponseEntity.ok("Usuario registrado y correo enviado");
+    }
 }
